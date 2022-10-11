b6b6a3374242b2c2d612ecbeb1c0812a099e9c86
Aishwarya-Hariharan-Iyer <e0893448@u.nus.edu>
2022-10-12 00:55:10 +0800

    Delete Type in Client

--- src/main/java/seedu/address/model/client/Type.java
+++
@@ -1,9 +0,0 @@
-package seedu.address.model.client;
-
-/**
- * Represents the types of clients for whom the project is developed as Employer, Organization (external) or
- * individual.
- */
-public enum Type {
-    INDIVIDUAL, ORGANIZATION, EMPLOYER, UNSPECIFIED
-}
